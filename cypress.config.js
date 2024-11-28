4181c29ae20562bce6d4909847c72dc271ab58c7
lhotovy <lhotovy@centrum.cz>
2024-11-28 10:08:26 +0100

    lighthouse setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "cypress";
+import { lighthouse, prepareAudit } from "@cypress-audit/lighthouse";
 
 export default defineConfig({
   projectId: "4strrf",
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on, config) {    
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        prepareAudit(launchOptions);
+      });
+
+      on("task", {
+        lighthouse: lighthouse(),
+        pa11y: pa11y(console.log.bind(console)),
+      });     
     },
   },
 });
